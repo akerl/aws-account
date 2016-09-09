1336603a2c594e3fdaecdb4532d34adf642379d7
Les Aker <me@lesaker.org>
2016-09-08 23:29:21 -0400

    set tumblr manually

--- terraform/kellywatts.com.tf
+++ terraform/kellywatts.com.tf
@@ -2,7 +2,14 @@ module "kellywatts_com" {
     source = "./domain"
     domain_name = "kellywatts.com"
     delegation_set_id = "${aws_route53_delegation_set.main.id}"
-    root_ipv4 = "66.6.44.4"
+}
+
+resource "aws_route53_record" "a_kellywatts_com" {
+    zone_id = "${module.kellywatts_com.zone_id}"
+    name = "kellywatts.com"
+    type = "A"
+    ttl = "86400"
+    records = ["66.6.44.4"]
 }
 
 resource "aws_route53_record" "cname_www_kellywatts_com" {
