5a2051c1675d2a25aa769fa42b174c22c74cc223
Les Aker <me@lesaker.org>
2016-09-08 23:52:10 -0400

    hyphens are back in style

--- zones/id-ed25519.pub.tf
+++ zones/id-ed25519.pub.tf
@@ -1,6 +1,6 @@
-module "id_ed25519_pub" {
+module "id-ed25519_pub" {
     source = "./domain"
-    domain_name = "id_ed25519.pub"
+    domain_name = "id-ed25519.pub"
     delegation_set_id = "${aws_route53_delegation_set.main.id}"
     root_ipv4 = "97.107.129.127"
     root_ipv6 = "2600:3c03:e001:9001::9001"
