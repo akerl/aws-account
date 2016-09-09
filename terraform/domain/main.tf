972caf4164f01f787b765b15b3303de3e31a85bc
Les Aker <me@lesaker.org>
2016-09-08 22:35:32 -0400

    fix var calls

--- terraform/domain/main.tf
+++ terraform/domain/main.tf
@@ -24,21 +24,21 @@ output "zone_id" {
 }
 
 resource "aws_route53_zone" "domain" {
-    delegation_set_id = "${delegation_set_id}"
-    name = "${domain_name}"
+    delegation_set_id = "${var.delegation_set_id}"
+    name = "${var.domain_name}"
 }
 
 resource "aws_route53_record" "soa" {
     zone_id = "${aws_route53_zone.domain.zone_id}"
-    name = "${domain_name}"
+    name = "${var.domain_name}"
     type = "SOA"
     ttl = "86400"
-    records = ["${aws_route53_zone.domain.name_servers.0} ${admin_email} 1 7200 900 1209600 3600"]
+    records = ["${aws_route53_zone.domain.name_servers.0} ${var.admin_email} 1 7200 900 1209600 3600"]
 }
 
 resource "aws_route53_record" "mx" {
     zone_id = "${aws_route53_zone.domain.zone_id}"
-    name = "${domain_name}"
+    name = "${var.domain_name}"
     type = "MX"
     ttl = "86400"
     records = [
@@ -52,7 +52,7 @@ resource "aws_route53_record" "mx" {
 
 resource "aws_route53_record" "spf" {
     zone_id = "${aws_route53_zone.domain.zone_id}"
-    name = "${domain_name}"
+    name = "${var.domain_name}"
     type = "SPF"
     ttl = "86400"
     records = ["v=spf1 a include:_spf.google.com ~all"]
@@ -60,7 +60,7 @@ resource "aws_route53_record" "spf" {
 
 resource "aws_route53_record" "txt" {
     zone_id = "${aws_route53_zone.domain.zone_id}"
-    name = "${domain_name}"
+    name = "${var.domain_name}"
     type = "TXT"
     ttl = "86400"
     records = ["v=spf1 a include:_spf.google.com ~all"]
@@ -68,32 +68,32 @@ resource "aws_route53_record" "txt" {
 
 resource "aws_route53_record" "a" {
     zone_id = "${aws_route53_zone.domain.zone_id}"
-    name = "${domain_name}"
+    name = "${var.domain_name}"
     type = "A"
     ttl = "86400"
-    records = ["${root_ipv4}"]
+    records = ["${var.root_ipv4}"]
 }
 
 resource "aws_route53_record" "aaaa" {
     zone_id = "${aws_route53_zone.domain.zone_id}"
-    name = "${domain_name}"
+    name = "${var.domain_name}"
     type = "AAAA"
     ttl = "86400"
-    records = ["${root_ipv6}"]
+    records = ["${var.root_ipv6}"]
 }
 
 resource "aws_route53_record" "www_a" {
     zone_id = "${aws_route53_zone.domain.zone_id}"
-    name = "www.${domain_name}"
+    name = "www.${var.domain_name}"
     type = "A"
     ttl = "86400"
-    records = ["${root_ipv4}"]
+    records = ["${var.root_ipv4}"]
 }
 
 resource "aws_route53_record" "www_aaaa" {
     zone_id = "${aws_route53_zone.domain.zone_id}"
-    name = "www.${domain_name}"
+    name = "www.${var.domain_name}"
     type = "AAAA"
     ttl = "86400"
-    records = ["${root_ipv6}"]
+    records = ["${var.root_ipv6}"]
 }
