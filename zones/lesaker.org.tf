ac52d7faf1eded31482401bf37c77eb01b5d3d97
Les Aker <me@lesaker.org>
2016-09-14 18:58:00 -0400

    fix extra root_ records

--- zones/lesaker.org.tf
+++ zones/lesaker.org.tf
@@ -2,8 +2,6 @@ module "lesaker_org" {
     source = "./domain"
     domain_name = "lesaker.org"
     delegation_set_id = "${aws_route53_delegation_set.main.id}"
-    root_ipv4 = "70.85.129.127"
-    root_ipv6 = "2600:3c00:e001:9001::1"
 }
 
 resource "aws_route53_record" "a_lesaker_org" {
