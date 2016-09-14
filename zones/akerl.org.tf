6c2d17afb262ede07169dfcac8e049386ca4341d
Les Aker <me@lesaker.org>
2016-09-14 15:46:50 -0400

    update blog URL

--- zones/akerl.org.tf
+++ zones/akerl.org.tf
@@ -10,16 +10,11 @@ resource "aws_route53_record" "a_blog_akerl_org" {
     zone_id = "${module.akerl_org.zone_id}"
     name = "blog.akerl.org"
     type = "A"
-    ttl = "86400"
-    records = ["70.85.129.127"]
-}
-
-resource "aws_route53_record" "aaaa_blog_akerl_org" {
-    zone_id = "${module.akerl_org.zone_id}"
-    name = "blog.akerl.org"
-    type = "AAAA"
-    ttl = "86400"
-    records = ["2600:3c00:e001:9001::1"]
+    alias {
+        name = "dvw7pswcn4cfg.cloudfront.net"
+        zone_id = "Z2FDTNDATAQYW2"
+        evaluate_target_health = false
+    }
 }
 
 resource "aws_route53_record" "a_committed_akerl_org" {
