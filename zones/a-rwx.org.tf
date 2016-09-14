5bae81abf13b91a57e97c8b5e27f54f308a6db93
Les Aker <me@lesaker.org>
2016-09-14 18:55:07 -0400

    remove miro

--- zones/a-rwx.org.tf
+++ zones/a-rwx.org.tf
@@ -6,22 +6,6 @@ module "a-rwx_org" {
     root_ipv6 = "2600:3c00:e001:9001::1"
 }
 
-resource "aws_route53_record" "a_miro_a-rwx_org" {
-    zone_id = "${module.a-rwx_org.zone_id}"
-    name = "miro.a-rwx.org"
-    type = "A"
-    ttl = "86400"
-    records = ["97.107.129.127"]
-}
-
-resource "aws_route53_record" "aaaa_miro_a-rwx_org" {
-    zone_id = "${module.a-rwx_org.zone_id}"
-    name = "miro.a-rwx.org"
-    type = "AAAA"
-    ttl = "86400"
-    records = ["2600:3c03:e001:9001::1"]
-}
-
 resource "aws_route53_record" "a_olhado_a-rwx_org" {
     zone_id = "${module.a-rwx_org.zone_id}"
     name = "olhado.a-rwx.org"
