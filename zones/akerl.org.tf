6d756f7e312713a55f21f0097fc16af1746ede6e
Les Aker <me@lesaker.org>
2016-09-14 15:57:09 -0400

    moar blog

--- zones/akerl.org.tf
+++ zones/akerl.org.tf
@@ -2,19 +2,24 @@ module "akerl_org" {
     source = "./domain"
     domain_name = "akerl.org"
     delegation_set_id = "${aws_route53_delegation_set.main.id}"
-    root_ipv4 = "70.85.129.127"
-    root_ipv6 = "2600:3c00:e001:9001::1"
 }
 
-resource "aws_route53_record" "a_blog_akerl_org" {
+resource "aws_route53_record" "a_akerl_org" {
     zone_id = "${module.akerl_org.zone_id}"
-    name = "blog.akerl.org"
+    name = "akerl.org"
     type = "A"
     alias {
         name = "dvw7pswcn4cfg.cloudfront.net"
         zone_id = "Z2FDTNDATAQYW2"
         evaluate_target_health = false
     }
+}
+
+resource "aws_route53_record" "a_blog_akerl_org" {
+    zone_id = "${module.akerl_org.zone_id}"
+    name = "blog.akerl.org"
+    type = "CNAME"
+    records = ["akerl.org."]
 }
 
 resource "aws_route53_record" "a_committed_akerl_org" {
