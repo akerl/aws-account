85ac672e34e6980ae6741dabeb58717651cabd13
Les Aker <me@lesaker.org>
2017-07-06 19:26:00 -0400

    add dns for internal servers/infra

--- akerl/dns/a-rwx.org.tf
+++ akerl/dns/a-rwx.org.tf
@@ -28,34 +28,131 @@ resource "aws_route53_record" "a_www_a-rwx_org" {
   }
 }
 
-resource "aws_route53_record" "a_osquery-c6_a-rwx_org" {
+resource "aws_route53_record" "a_alfa.internal_a-rwx_org" {
   zone_id = "${module.a-rwx_org.zone_id}"
-  name    = "osquery-c6.a-rwx.org"
+  name    = "alfa.internal.a-rwx.org"
   type    = "A"
   ttl     = "86400"
-  records = ["45.79.181.133"]
+  records = ["10.1.1.1"]
 }
 
-resource "aws_route53_record" "a_osquery-c7_a-rwx_org" {
+resource "aws_route53_record" "a_alfa-ipmi.internal_a-rwx_org" {
   zone_id = "${module.a-rwx_org.zone_id}"
-  name    = "osquery-c7.a-rwx.org"
+  name    = "alfa-ipmi.internal.a-rwx.org"
   type    = "A"
   ttl     = "86400"
-  records = ["104.200.28.22"]
+  records = ["10.0.1.1"]
 }
 
-resource "aws_route53_record" "a_osquery-ubuntu_a-rwx_org" {
+resource "aws_route53_record" "a_bravo.internal_a-rwx_org" {
   zone_id = "${module.a-rwx_org.zone_id}"
-  name    = "osquery-ubuntu.a-rwx.org"
+  name    = "bravo.internal.a-rwx.org"
   type    = "A"
   ttl     = "86400"
-  records = ["104.200.28.27"]
+  records = ["10.1.1.2"]
 }
 
-resource "aws_route53_record" "a_osquery-arch_a-rwx_org" {
+resource "aws_route53_record" "a_bravo-ipmi.internal_a-rwx_org" {
   zone_id = "${module.a-rwx_org.zone_id}"
-  name    = "osquery-arch.a-rwx.org"
+  name    = "bravo-ipmi.internal.a-rwx.org"
   type    = "A"
   ttl     = "86400"
-  records = ["104.200.28.35"]
+  records = ["10.0.1.2"]
 }
+
+resource "aws_route53_record" "a_charlie.internal_a-rwx_org" {
+  zone_id = "${module.a-rwx_org.zone_id}"
+  name    = "charlie.internal.a-rwx.org"
+  type    = "A"
+  ttl     = "86400"
+  records = ["10.1.1.3"]
+}
+
+resource "aws_route53_record" "a_charlie-ipmi.internal_a-rwx_org" {
+  zone_id = "${module.a-rwx_org.zone_id}"
+  name    = "charlie-ipmi.internal.a-rwx.org"
+  type    = "A"
+  ttl     = "86400"
+  records = ["10.0.1.3"]
+}
+
+resource "aws_route53_record" "a_delta.internal_a-rwx_org" {
+  zone_id = "${module.a-rwx_org.zone_id}"
+  name    = "delta.internal.a-rwx.org"
+  type    = "A"
+  ttl     = "86400"
+  records = ["10.1.1.4"]
+}
+
+resource "aws_route53_record" "a_delta-ipmi.internal_a-rwx_org" {
+  zone_id = "${module.a-rwx_org.zone_id}"
+  name    = "delta-ipmi.internal.a-rwx.org"
+  type    = "A"
+  ttl     = "86400"
+  records = ["10.0.1.4"]
+}
+
+resource "aws_route53_record" "a_echo.internal_a-rwx_org" {
+  zone_id = "${module.a-rwx_org.zone_id}"
+  name    = "echo.internal.a-rwx.org"
+  type    = "A"
+  ttl     = "86400"
+  records = ["10.1.1.5"]
+}
+
+resource "aws_route53_record" "a_echo-ipmi.internal_a-rwx_org" {
+  zone_id = "${module.a-rwx_org.zone_id}"
+  name    = "echo-ipmi.internal.a-rwx.org"
+  type    = "A"
+  ttl     = "86400"
+  records = ["10.0.1.5"]
+}
+
+resource "aws_route53_record" "a_foxtrot.internal_a-rwx_org" {
+  zone_id = "${module.a-rwx_org.zone_id}"
+  name    = "foxtrot.internal.a-rwx.org"
+  type    = "A"
+  ttl     = "86400"
+  records = ["10.1.1.6"]
+}
+
+resource "aws_route53_record" "a_foxtrot-ipmi.internal_a-rwx_org" {
+  zone_id = "${module.a-rwx_org.zone_id}"
+  name    = "foxtrot-ipmi.internal.a-rwx.org"
+  type    = "A"
+  ttl     = "86400"
+  records = ["10.0.1.6"]
+}
+
+resource "aws_route53_record" "a_edge.internal_a-rwx_org" {
+  zone_id = "${module.a-rwx_org.zone_id}"
+  name    = "edge.internal.a-rwx.org"
+  type    = "A"
+  ttl     = "86400"
+  records = ["10.0.0.1"]
+}
+
+resource "aws_route53_record" "a_switch1.internal_a-rwx_org" {
+  zone_id = "${module.a-rwx_org.zone_id}"
+  name    = "switch1.internal.a-rwx.org"
+  type    = "A"
+  ttl     = "86400"
+  records = ["10.0.0.2"]
+}
+
+resource "aws_route53_record" "a_jump.internal_a-rwx_org" {
+  zone_id = "${module.a-rwx_org.zone_id}"
+  name    = "jump.internal.a-rwx.org"
+  type    = "A"
+  ttl     = "86400"
+  records = ["10.2.0.1"]
+}
+
+resource "aws_route53_record" "a_infra.internal_a-rwx_org" {
+  zone_id = "${module.a-rwx_org.zone_id}"
+  name    = "infra.internal.a-rwx.org"
+  type    = "A"
+  ttl     = "86400"
+  records = ["10.0.0.100"]
+}
+
