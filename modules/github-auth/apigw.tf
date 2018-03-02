608d76fe5ff54a0abc1a950eac11db4338107cf0
Les Aker <me@lesaker.org>
2018-03-01 23:39:36 -0500

    update apigw

--- modules/github-auth/apigw.tf
+++ modules/github-auth/apigw.tf
@@ -4,9 +4,25 @@ resource "aws_api_gateway_rest_api" "api" {
 
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "root_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
   resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
   http_method   = "ANY"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "root_integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  http_method             = "${aws_api_gateway_method.method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.github-auth-lambda.invoke_arn}"
 }
 
 resource "aws_api_gateway_method_settings" "settings" {
@@ -22,7 +38,7 @@ resource "aws_api_gateway_method_settings" "settings" {
 
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
-  resource_id             = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  resource_id             = "${aws_api_gateway_resource.resource.id}"
   http_method             = "${aws_api_gateway_method.method.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -32,7 +48,10 @@ resource "aws_api_gateway_integration" "integration" {
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
     "aws_api_gateway_method.method",
+    "aws_api_gateway_method.root_method",
     "aws_api_gateway_integration.integration",
+    "aws_api_gateway_integration.root_integration",
+    "aws_api_gateway_resource.resource",
   ]
 
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
@@ -41,6 +60,12 @@ resource "aws_api_gateway_deployment" "deployment" {
   variables {
     bucket = "${var.data-bucket}"
   }
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  path_part   = "{path+}"
 }
 
 resource "aws_api_gateway_domain_name" "domain" {
