9ce3bf491b7dcf4bacd71cdd6b3b1be4d863cfb3
Les Aker <me@lesaker.org>
2018-05-09 19:33:00 -0400

    add data events for cloudtrail, fixes #16

--- account/cloudtrail.tf
+++ account/cloudtrail.tf
@@ -4,6 +4,26 @@ resource "aws_cloudtrail" "main-trail" {
   include_global_service_events = true
   is_multi_region_trail         = true
   enable_log_file_validation    = true
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+
+    data_resource {
+      type   = "AWS::S3::Object"
+      values = ["arn:aws:s3:::"]
+    }
+  }
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+
+    data_resource {
+      type   = "AWS::Lambda::Function"
+      values = ["arn:aws:lambda"]
+    }
+  }
 }
 
 resource "aws_s3_bucket" "main-trail" {
