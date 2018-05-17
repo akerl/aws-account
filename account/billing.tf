bdfe0e85a84c9067d35ace41c11810dcb1fc32dd
Les Aker <me@lesaker.org>
2018-05-16 22:33:24 -0700

    fix formatting

--- account/billing.tf
+++ account/billing.tf
@@ -1,8 +1,8 @@
 resource "aws_budgets_budget" "cost" {
-  name                  = "cost_budget"
-  budget_type           = "COST"
-  limit_amount          = "10"
-  limit_unit            = "USD"
-  time_unit             = "MONTHLY"
-  time_period_start     = "2017-01-01_00:00"
+  name              = "cost_budget"
+  budget_type       = "COST"
+  limit_amount      = "10"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+  time_period_start = "2017-01-01_00:00"
 }
