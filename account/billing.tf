5f6153740b6a8dc843a398828b9ca4536e6dadf7
Les Aker <me@lesaker.org>
2018-05-16 22:28:44 -0700

    update state

--- account/billing.tf
+++ account/billing.tf
@@ -1,25 +1,8 @@
-resource "aws_cloudwatch_metric_alarm" "billing-alarm" {
-  alarm_name          = "billing-alarm"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "EstimatedCharges"
-  namespace           = "AWS/Billing"
-  period              = "21600"
-  statistic           = "Maximum"
-  threshold           = "20"
-
-  dimensions {
-    Currency = "USD"
-  }
-
-  alarm_description         = "Check for billing spikes"
-  alarm_actions             = ["${module.sns-to-slack.sns-topic-arn}"]
-  insufficient_data_actions = ["${module.sns-to-slack.sns-topic-arn}"]
-}
-
-module "sns-to-slack" {
-  source         = "../modules/sns-to-slack"
-  topic          = "billing-alarm"
-  config-bucket  = "akerl-billing-alarm"
-  logging-bucket = "${aws_s3_bucket.logging.id}"
+resource "aws_budgets_budget" "cost" {
+  name                  = "cost_budget"
+  budget_type           = "COST"
+  limit_amount          = "10"
+  limit_unit            = "USD"
+  time_unit             = "MONTHLY"
+  time_period_start     = "2017-01-01_00:00"
 }
