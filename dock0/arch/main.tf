cc3abe893f64f554b4e1ff182c2aa82558fec8b7
Les Aker <me@lesaker.org>
2018-09-19 20:00:06 -0400

    crank up the frequency

--- dock0/arch/main.tf
+++ dock0/arch/main.tf
@@ -2,7 +2,7 @@ module "hookshot" {
   source         = "../../modules/hookshot"
   logging-bucket = "${var.logging-bucket}"
   data-bucket    = "dock0-arch"
-  rate           = "12 hours"
+  rate           = "1 hours"
 }
 
 variable "logging-bucket" {
