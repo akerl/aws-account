29d59a00943defa139dbb10f6ed38384d63a8643
Les Aker <me@lesaker.org>
2019-03-11 22:00:01 +0300

    update hookshot

--- hookshot.tf
+++ hookshot.tf
@@ -1,6 +1,6 @@
 module "hourly" {
   source         = "armorfret/lambda-hookshot/aws"
-  version        = "0.0.6"
+  version        = "0.0.7"
   logging_bucket = "${module.account.logging_bucket}"
   lambda_bucket  = "akerl-hookshot"
   config_bucket  = "hookshot-hourly"
