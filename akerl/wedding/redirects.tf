23b2ce1f5ecc0f7848a4832c7fb408630af916e6
Les Aker <me@lesaker.org>
2020-08-30 05:09:17 +0000

    update acm module

--- akerl/wedding/redirects.tf
+++ akerl/wedding/redirects.tf
@@ -97,7 +97,7 @@ resource "aws_cloudfront_distribution" "redirect_distribution" {
 
 module "certificate" {
   source    = "armorfret/acm-certificate/aws"
-  version   = "0.1.4"
+  version   = "0.1.5"
   hostnames = var.domains
 }
 
--- s3authproxy.tf
+++ s3authproxy.tf
@@ -1,6 +1,6 @@
 module "akerl-private-files" {
   source         = "armorfret/lambda-s3authproxy/aws"
-  version        = "0.2.2"
+  version        = "0.2.3"
   logging_bucket = module.account.logging_bucket
   config_bucket  = "akerl-private-files-config"
   data_bucket    = "akerl-private-files"
@@ -10,7 +10,7 @@ module "akerl-private-files" {
 
 module "akerl-coolquotes-share" {
   source         = "armorfret/lambda-s3authproxy/aws"
-  version        = "0.2.2"
+  version        = "0.2.3"
   logging_bucket = module.account.logging_bucket
   config_bucket  = "akerl-coolquotes-share-config"
   data_bucket    = "akerl-coolquotes-share"
