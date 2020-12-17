aa6cd2a357641c4b5334465b87289654dac4bcf6
Les Aker <me@lesaker.org>
2020-12-17 08:51:40 -0500

    update state

--- akerl/dns/akerl.dev.tf
+++ akerl/dns/akerl.dev.tf
@@ -5,3 +5,12 @@ module "akerl_dev" {
   domain_name       = "akerl.dev"
   delegation_set_id = aws_route53_delegation_set.main.id
 }
+
+resource "aws_route53_record" "pumidor_temp_record" {
+  zone_id = module.akerl_dev.zone_id
+  name    = "pumidor.akerl.dev"
+  type    = "A"
+  ttl     = "60"
+  records = ["162.216.18.19"]
+}
+
