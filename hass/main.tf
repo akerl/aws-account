3201fb06d6913954f72546ffde4ec0d31d5dfb26
Les Aker <akerl@Less-MacBook.standard.home.a-rwx.org>
2020-12-18 23:12:22 -0500

    add hass lambda

--- hass/main.tf
+++ hass/main.tf
@@ -26,3 +26,25 @@ resource "aws_iam_role_policy_attachment" "lambda" {
   role       = aws_iam_role.lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_lambda_function" "lambda" {
+  filename      = "payload.zip"
+  function_name = "hass"
+  role          = aws_iam_role.lambda.arn
+  handler       = "lambda_function.lambda_handler"
+  source_code_hash = filebase64sha256("payload.zip")
+  runtime = "python3.8"
+  environment {
+    variables = {
+      BASE_URL = "https://hass.a-rwx.org
+    }
+  }
+}
+
+resource "aws_lambda_permission" "allow_alexa" {
+  statement_id  = "AllowExecutionFromAlexa"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "alexa-appkit.amazon.com"
+  event_source_token = "amzn1.ask.skill.abac7b2d-2bfd-4952-8501-efe3206d0b23"
+}
