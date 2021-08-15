de096f357b07e317a66715064a52411a1525d6e0
Les Aker <me@lesaker.org>
2021-08-15 00:21:36 -0400

    ses setup

--- main.tf
+++ main.tf
@@ -60,3 +60,7 @@ module "akerl-aws-account" {
 module "hass" {
   source = "./hass"
 }
+
+module "nas" {
+  source = "./nas"
+}
---
+++ nas/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "3.4.0"
+    }
+
+    awscreds = {
+      source  = "terraform.scrtybybscrty.org/armorfret/awscreds"
+      version = "0.2.0"
+    }
+  }
+}
+
+resource "aws_ses_domain_identity" "this" {
+  domain = "a-rwx.org"
+}
+
+resource "aws_route53_record" "verification" {
+  zone_id = "Z1GSN0DUY7LO3I"
+  name    = "_amazonses.${aws_ses_domain_identity.this.domain}"
+  type    = "TXT"
+  ttl     = "600"
+  records = [aws_ses_domain_identity.this.verification_token]
+}
+
+resource "aws_ses_domain_dkim" "this" {
+  domain = aws_ses_domain_identity.this.domain
+}
+
+resource "aws_route53_record" "dkim" {
+  count   = 3
+  zone_id = aws_route53_record.verification.zone_id
+  name    = "${element(aws_ses_domain_dkim.this.dkim_tokens, count.index)}._domainkey"
+  type    = "CNAME"
+  ttl     = "600"
+  records = ["${element(aws_ses_domain_dkim.this.dkim_tokens, count.index)}.dkim.amazonses.com"]
+}
+
+data "aws_iam_policy_document" "ses_policy" {
+  statement {
+    actions   = ["ses:SendRawEmail"]
+    resources = [aws_ses_domain_identity.this.arn]
+  }
+}
+
+resource "awscreds_iam_access_key" "this" {
+  user = aws_iam_user.this.name
+  file = "creds/${aws_iam_user.this.name}"
+}
+
+resource "aws_iam_user" "this" {
+  name = "ses-sender"
+}
+
+resource "aws_iam_user_policy" "send_emails" {
+  policy = data.aws_iam_policy_document.ses_policy.json
+  user   = aws_iam_user.this.name
+}
