367f16677a082aa42e3db28cc3b4f4b2e48a8eb4
Les Aker <me@lesaker.org>
2022-08-06 08:05:48 -0400

    update state

--- akerl/dns/a-rwx.org.tf
+++ akerl/dns/a-rwx.org.tf
@@ -181,14 +181,6 @@ resource "aws_route53_record" "logs_a-rwx_org" {
   records = ["10.0.1.113"]
 }
 
-resource "aws_route53_record" "log-ingest_a-rwx_org" {
-  zone_id = module.a-rwx_org.zone_id
-  name    = "log-ingest.a-rwx.org"
-  type    = "A"
-  ttl     = "60"
-  records = ["10.0.1.113"]
-}
-
 resource "aws_route53_record" "dmz_ext_linode_a-rwx_org" {
   for_each = toset(local.ext_records)
   zone_id  = module.a-rwx_org.zone_id
@@ -264,15 +256,6 @@ module "logs_validation" {
   parent_zone_id    = module.a-rwx_org.zone_id
 }
 
-module "log-ingest_validation" {
-  source            = "armorfret/r53-certbot/aws"
-  version           = "0.1.1"
-  admin_email       = var.admin_email
-  delegation_set_id = "log-ingest"
-  subzone_name      = "log-ingest.certs.a-rwx.org"
-  cert_name         = "log-ingest.a-rwx.org"
-  parent_zone_id    = module.a-rwx_org.zone_id
-}
 module "pumidor_ext_validation" {
   source            = "armorfret/r53-certbot/aws"
   version           = "0.1.1"
