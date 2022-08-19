de45e5d44812c1f1b10c9e39539487fcc5c270fc
Les Aker <me@lesaker.org>
2022-08-19 00:23:04 -0400

    remove hookshot

--- hookshot.tf
+++
@@ -1,8 +0,0 @@
-module "hourly" {
-  source         = "armorfret/lambda-hookshot/aws"
-  version        = "0.2.3"
-  logging_bucket = module.account.logging_bucket
-  lambda_bucket  = "akerl-hookshot"
-  config_bucket  = "hookshot-hourly"
-}
-
