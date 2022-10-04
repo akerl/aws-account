378bd0fe93dc5bff7a2522892ccad24014eb6df9
Les Aker <me@lesaker.org>
2022-10-03 22:53:20 -0400

    update watchdog

--- watchdog.tf
+++ watchdog.tf
@@ -1,6 +1,6 @@
 module "akerl-watchdog-site" {
   source         = "armorfret/lambda-watchdog/aws"
-  version        = "0.1.3"
+  version        = "0.1.4"
   logging_bucket = module.account.logging_bucket
   config_bucket  = "akerl-watchdog-site"
   data_bucket    = "akerl-watchdog-site"
