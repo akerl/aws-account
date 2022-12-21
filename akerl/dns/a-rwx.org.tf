00c764af0b32d070220788768621dce9c0c7be55
Les Aker <me@lesaker.org>
2022-12-21 11:25:53 -0500

    update state

--- akerl/dns/a-rwx.org.tf
+++ akerl/dns/a-rwx.org.tf
@@ -73,7 +73,7 @@ locals {
     "172.16.1.11"  = "cam-family.iot"
     "172.16.1.12"  = "cam-living.iot"
     "172.16.1.13"  = "cam-basement.iot"
-    "172.16.1.18"  = "kiosk-bar.iot"
+    "172.16.1.18"  = "kiosk-zero.iot"
     "172.16.1.19"  = "kiosk-tea.iot"
     "172.16.1.20"  = "thermostat-bar.iot"
     "172.16.1.21"  = "thermostat-basement.iot"
