046eec2943fd553748f317513c93579504ce0f82
Les Aker <me@lesaker.org>
2023-05-12 20:18:45 -0400

    update domains

--- dns/phishing.network.tf
+++
@@ -1,8 +0,0 @@
-module "phishing_network" {
-  source            = "armorfret/r53-zone/aws"
-  version           = "0.5.0"
-  admin_email       = var.admin_email
-  domain_name       = "phishing.network"
-  delegation_set_id = aws_route53_delegation_set.main.id
-}
-
--- dns/suspicious.cloud.tf
+++
@@ -1,8 +0,0 @@
-module "suspicious_cloud" {
-  source            = "armorfret/r53-zone/aws"
-  version           = "0.5.0"
-  admin_email       = var.admin_email
-  domain_name       = "suspicious.cloud"
-  delegation_set_id = aws_route53_delegation_set.main.id
-}
-
