c65942319e3d8b966b64484bb454b44faee5a6ec
Les Aker <me@lesaker.org>
2023-06-03 20:12:37 -0400

    switch to separate photos bucket

--- frame.tf
+++ frame.tf
@@ -3,7 +3,7 @@ module "akerl-frame-site" {
   version        = "0.0.2"
   logging_bucket = aws_s3_bucket.logging.id
   config_bucket  = "akerl-frame-data"
-  data_bucket    = "akerl-frame-data"
+  data_bucket    = "akerl-frame-photos"
   lambda_bucket  = module.akerl-frame.publish_bucket
   hostname       = "frame.akerl.app"
 }
