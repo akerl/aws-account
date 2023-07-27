c02e417640b095dbe88912b6a5a27f9e5331ad5e
Les Aker <me@lesaker.org>
2023-07-26 21:18:16 -0400

    add domains output

--- main.tf
+++ main.tf
@@ -74,6 +74,10 @@ data "terraform_remote_state" "unifi" {
   }
 }
 
+output "domains" {
+  value = var.domains
+}
+
 output "nameservers" {
   value = aws_route53_delegation_set.main.name_servers
 }
