be6deeb44360b735c8f0f3301358e0dcb0281ea3
Les Aker <me@lesaker.org>
2023-08-01 22:00:49 -0400

    update modules

--- blindclock.tf
+++ blindclock.tf
@@ -1,6 +1,6 @@
 module "akerl-blindclock-site" {
   source         = "armorfret/lambda-blindclock/aws"
-  version        = "0.2.2"
+  version        = "0.2.3"
   logging_bucket = aws_s3_bucket.logging.id
   config_bucket  = "akerl-blindclock-data"
   data_bucket    = "akerl-blindclock-data"
