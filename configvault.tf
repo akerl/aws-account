72a56c7f2ed824ac89b64a9dcef05293daa975a4
Les Aker <me@lesaker.org>
2023-12-31 20:19:00 -0500

    update configvault

--- configvault.tf
+++ configvault.tf
@@ -1,6 +1,6 @@
 module "puppet-vault" {
   source         = "armorfret/s3-configvault/aws"
-  version        = "0.3.2"
+  version        = "0.3.3"
   prefix         = "puppet"
   vault_bucket   = "akerl-puppet"
   logging_bucket = aws_s3_bucket.logging.id
