fc3a7ee00a592fd48ecf7b49729368ab90e72805
Les Aker <me@lesaker.org>
2024-11-28 13:07:57 -0500

    add heracles

--- configvault.tf
+++ configvault.tf
@@ -22,5 +22,6 @@ module "puppet-vault" {
     "proxy",
     "mqtt",
     "ledgerdb",
+    "heracles",
   ]
 }
--- servers.tf
+++ servers.tf
@@ -90,6 +90,14 @@ module "metrics_validation" {
   zone_id     = module.zones["a-rwx.org"].zone_id
 }
 
+module "heracles_validation" {
+  source      = "armorfret/r53-certbot/aws"
+  version     = "0.6.4"
+  admin_email = var.admin_email
+  cert_name   = "heracles.servers.home.a-rwx.org"
+  zone_id     = module.zones["a-rwx.org"].zone_id
+}
+
 module "grafana_validation" {
   source      = "armorfret/r53-certbot/aws"
   version     = "0.6.4"
