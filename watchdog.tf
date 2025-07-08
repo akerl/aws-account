d5981c0cd3762316b53af3eaf1dc5697c7f02ea6
Les Aker <me@lesaker.org>
2025-07-07 22:13:36 -0400

    remove osrs bot

--- watchdog.tf
+++ watchdog.tf
@@ -21,28 +21,3 @@ resource "aws_route53_record" "a_watchdog_akerl_app" {
     evaluate_target_health = false
   }
 }
-
-module "akerl-osrs-site" {
-  source         = "armorfret/lambda-watchdog/aws"
-  version        = "0.8.0"
-  logging_bucket = aws_s3_bucket.logging.id
-  config_bucket  = "akerl-osrs-site"
-  data_bucket    = "akerl-osrs-site"
-  lambda_bucket  = module.akerl-watchdog.publish_bucket
-  hostname       = "osrs.akerl.app"
-  alert_email    = "me@lesaker.org"
-  rate           = 300
-}
-
-resource "aws_route53_record" "a_osrs_akerl_app" {
-  zone_id = module.zones["akerl.app"].zone_id
-
-  name = "osrs.akerl.app"
-  type = "A"
-
-  alias {
-    name                   = module.akerl-osrs-site.dns_name
-    zone_id                = module.akerl-blog.cloudfront_zone_id
-    evaluate_target_health = false
-  }
-}
