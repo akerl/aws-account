dc6e1156734b2cc2b85d1021b14c5b79653d6662
Les Aker <me@lesaker.org>
2025-09-07 10:59:36 -0400

    clean up DNS code

--- dns.tf
+++ dns.tf
@@ -8,6 +8,11 @@ locals {
     "baby",
   ]
 
+  hass_records = [
+    "zwave.a-rwx.org",
+    "frameproxy.a-rwx.org",
+  ]
+
   wg_records = {
     "10.255.255.1" : "dmz",
     "10.255.255.2" : "hub",
@@ -16,7 +21,13 @@ locals {
     "10.255.255.5" : "codepad",
     "10.255.255.6" : "goat",
     "10.255.255.7" : "charts",
+    "10.255.255.8" : "heracles",
     "10.255.255.9" : "proxy",
+  }
+
+  linode_aliases = {
+    "goat.akerl.app" : "goat",
+    "charts.akerl.app" : "charts",
   }
 }
 
@@ -39,7 +50,7 @@ resource "aws_route53_record" "linode" {
   records = [each.value]
 }
 
-resource "aws_route53_record" "dmz_hub_linode_a-rwx_org" {
+resource "aws_route53_record" "alias_hub_a-rwx_org" {
   for_each = toset(local.hub_records)
   zone_id  = module.zones["a-rwx.org"].zone_id
   name     = "${each.key}.a-rwx.org"
@@ -56,4 +67,23 @@ resource "aws_route53_record" "dmz_int_a-rwx_org" {
   type    = "A"
   ttl     = "60"
   records = [each.key]
+}
+
+resource "aws_route53_record" "alias_hass_a-rwx_org" {
+  for_each = toset(local.hass_records)
+  zone_id  = module.zones["a-rwx.org"].zone_id
+  name     = each.key
+  type     = "A"
+  ttl      = "60"
+  records  = ["10.0.1.80"]
+}
+
+resource "aws_route53_record" "linode_aliases" {
+  for_each = local.linode_aliases
+
+  zone_id = module.zones[replace(each.key, local.host_to_zone_regex, "$1")].zone_id
+  name    = each.key
+  type    = "A"
+  ttl     = "60"
+  records = [data.terraform_remote_state.linode.outputs.instance_addresses[each.value]]
 }
--- servers.tf
+++ servers.tf
@@ -1,205 +1,36 @@
-resource "aws_route53_record" "zwave_a-rwx_org" {
-  zone_id = module.zones["a-rwx.org"].zone_id
-  name    = "zwave.a-rwx.org"
-  type    = "A"
-  ttl     = "60"
-  records = ["10.0.1.80"]
+locals {
+  certificates = {
+    "goat.akerl.app" : null,
+    "charts.akerl.app" : null,
+    "frameproxy.a-rwx.org" : null,
+    "frame.akerl.app" : ["letsencrypt.org; validationmethods=dns-01", "amazon.com"],
+    "syslog.servers.home.a-rwx.org" : null,
+    "metrics.servers.home.a-rwx.org" : null,
+    "influx.servers.home.a-rwx.org" : null,
+    "heracles.servers.home.a-rwx.org" : null,
+    "grafana.servers.home.a-rwx.org" : null,
+    "printer.standard.home.a-rwx.org" : null,
+    "nas.servers.home.a-rwx.org" : null,
+    "hass.servers.home.a-rwx.org" : null,
+    "baby.servers.home.a-rwx.org" : null,
+    "logs.a-rwx.org" : null,
+    "metrics.a-rwx.org" : null,
+    "grafana.a-rwx.org" : null,
+    "nvr.a-rwx.org" : null,
+    "hass.a-rwx.org" : null,
+    "baby.a-rwx.org" : null,
+    "zwave.a-rwx.org" : null,
+    "gateway.infra.home.a-rwx.org" : null,
+  }
 }
 
-resource "aws_route53_record" "a_goat_akerl_app" {
-  zone_id = module.zones["akerl.app"].zone_id
-  name    = "goat.akerl.app"
-  type    = "A"
-  ttl     = "60"
-  records = [data.terraform_remote_state.linode.outputs.instance_addresses["goat"]]
-}
-
-module "goat_ext_validation" {
+module "certificate_validation" {
+  for_each    = local.certificates
   source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
+  version     = "0.6.5"
   admin_email = var.admin_email
-  cert_name   = "goat.akerl.app"
-  zone_id     = module.zones["akerl.app"].zone_id
-}
-
-resource "aws_route53_record" "a_charts_akerl_app" {
-  zone_id = module.zones["akerl.app"].zone_id
-  name    = "charts.akerl.app"
-  type    = "A"
-  ttl     = "60"
-  records = [data.terraform_remote_state.linode.outputs.instance_addresses["charts"]]
-}
-
-module "charts_ext_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "charts.akerl.app"
-  zone_id     = module.zones["akerl.app"].zone_id
-}
-
-module "frameproxy_proxy_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "frameproxy.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-resource "aws_route53_record" "frameproxy_a-rwx_org" {
-  zone_id = module.zones["a-rwx.org"].zone_id
-  name    = "frameproxy.a-rwx.org"
-  type    = "A"
-  ttl     = "60"
-  records = ["10.0.1.80"]
-}
-
-module "frameproxy_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "frame.akerl.app"
-  zone_id     = module.zones["akerl.app"].zone_id
-  issue_list = [
-    "letsencrypt.org; validationmethods=dns-01",
-    "amazon.com",
-  ]
-}
-
-module "syslog_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "syslog.servers.home.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "metrics_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "metrics.servers.home.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "influx_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "influx.servers.home.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "heracles_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "heracles.servers.home.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "grafana_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "grafana.servers.home.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "printer_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "printer.standard.home.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "nas_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "nas.servers.home.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "hass_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "hass.servers.home.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "baby_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "baby.servers.home.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "logs_ext_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "logs.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "metrics_ext_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "metrics.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "grafana_ext_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "grafana.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "nvr_ext_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "nvr.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "hass_ext_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "hass.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "baby_ext_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "baby.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-
-module "zwave_ext_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "zwave.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
-}
-
-module "gateway_validation" {
-  source      = "armorfret/r53-certbot/aws"
-  version     = "0.6.4"
-  admin_email = var.admin_email
-  cert_name   = "gateway.infra.home.a-rwx.org"
-  zone_id     = module.zones["a-rwx.org"].zone_id
+  cert_name   = each.key
+  zone_id     = module.zones[replace(each.key, local.host_to_zone_regex, "$1")].zone_id
+  issue_list  = each.value
 }
 
